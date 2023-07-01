71049cd5252c4052c394e3104e29ed3de5a7c671
Cameron Burns-Hunter <Cameron.burnshunter@sky.uk>
2023-07-01 19:05:10 +0100

    remove try block

--- python_service/config/__init__.py
+++ python_service/config/__init__.py
@@ -1,9 +1,9 @@
 import yaml, sys
 
-try:
+local_flag = ''
+
+if len(sys.argv) > 1:
     local_flag = sys.argv[1]
-except IndexError:
-    local_flag = ''
 
 def load_config():
     with open('config/default.yaml', 'r') as file:
