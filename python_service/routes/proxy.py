fdeb952f99b3b1ba77c95c0ef4f57011db62ea6c
Cameron Burns-Hunter <Cameron.burnshunter@sky.uk>
2023-07-02 15:03:00 +0100

    add basic ingress

--- python_service/routes/proxy.py
+++ python_service/routes/proxy.py
@@ -1,14 +1,14 @@
 
 from flask import Blueprint
 import requests
-from config import config
+# from config import config
 
 _proxy = Blueprint('proxy', __name__)
 
-@_proxy.route('/java_service/cameron', methods=['GET'])
-def proxy():
-    proxy_host = config['proxy_host']
-    target_url = f'http://{proxy_host}/api/users/cameron'
+@_proxy.route('/proxy/<hostname>', methods=['GET'])
+def proxy(hostname):
+    # proxy_host = config['proxy_host']
+    target_url = f'http://{hostname}/api/users/cameron'
 
     response = requests.request(
         method="GET",
